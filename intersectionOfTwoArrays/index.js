8188150c1c8c5a59f5d0841020a11ca428cbcf77
makati <Maksim.Atiuganov@sitronics-kt.ru>
2024-01-13 16:25:32 +0400

    Решение задачи

---
+++ intersectionOfTwoArrays/index.js
@@ -0,0 +1,16 @@
+/*
+https://leetcode.com/problems/intersection-of-two-arrays/
+
+Example 1:
+Input: nums1 = [1,2,2,1], nums2 = [2,2]
+Output: [2]
+
+Example 2:
+Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+Output: [9,4]
+Explanation: [4,9] is also accepted.
+*/
+
+function intersection(nums1, nums2) {
+  return [...new Set(nums1.filter(item => nums2.includes(item)))]
+}
